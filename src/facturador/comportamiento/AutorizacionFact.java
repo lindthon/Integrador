ec4571a0bd2de76945d9d9d556ce4ce00d5cf9f3
NeythanLevanest <neythan.astro@gmail.com>
2018-12-18 22:49:10 -0500

    actualizacion main

---
+++ src/facturador/comportamiento/AutorizacionFact.java
@@ -0,0 +1,5 @@
+package facturador.comportamiento;
+
+public class AutorizacionFact {
+
+}
--- src/facturador/main/FacturadorMain.java
+++ src/facturador/main/FacturadorMain.java
@@ -1,5 +1,7 @@
 package facturador.main;
 
+import java.util.Scanner;
+
 import facturador.creacional.ComprobanteElectronico;
 import facturador.creacional.ComprobantesFactory;
 
@@ -7,10 +9,29 @@ public class FacturadorMain {
 
 	public static void main(String[] args) {
 		ComprobantesFactory comprobanteFact = new ComprobantesFactory();
-		ComprobanteElectronico factura = comprobanteFact.getComprobante("FACTURA");
-		ComprobanteElectronico guiaRemision = comprobanteFact.getComprobante("GUIAREMISION");
-		ComprobanteElectronico notaCredito = comprobanteFact.getComprobante("NOTACREDITO");
-		
+	
+		System.out.println("Seleccione el comprobante que desea crear: \n"
+				+ "Escoja 1 para crear Factura\n"
+				+ "Escoja 2 para crear Guia de remision\n"
+				+ "Escoja 3 para crear Nota de Credito\n");
+				
+		Scanner sc = new Scanner(System.in);
+		int valor = sc.nextInt();
+		switch (valor) {
+		case 1:
+			ComprobanteElectronico factura = comprobanteFact.getComprobante("FACTURA");
+			break;
+		case 2:
+			ComprobanteElectronico guiaRemision = comprobanteFact.getComprobante("GUIAREMISION");
+			break;
+		case 3:
+			ComprobanteElectronico notaCredito = comprobanteFact.getComprobante("NOTACREDITO");
+			break;
+
+		default:
+			break;
+		}
+			
 		
 	}
 

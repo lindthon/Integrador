c0129609dba2be3aee6156e78b8165d55efc8dcd
NeythanLevanest <neythan.astro@gmail.com>
2018-12-18 23:18:01 -0500

    Prueba de implementacion con una factura

--- src/facturador/main/FacturadorMain.java
+++ src/facturador/main/FacturadorMain.java
@@ -2,8 +2,13 @@ package facturador.main;
 
 import java.util.Scanner;
 
+import facturador.comportamiento.AutorizacionFact;
+import facturador.comportamiento.AutorizadorSRI;
+import facturador.comportamiento.Esquema;
 import facturador.creacional.ComprobanteElectronico;
 import facturador.creacional.ComprobantesFactory;
+import facturador.estructural.IComprobante;
+import facturador.estructural.LogoDecorator;
 
 public class FacturadorMain {
 
@@ -22,12 +27,33 @@ public class FacturadorMain {
 		switch (valor) {
 		case 1:
 			ComprobanteElectronico factura = comprobanteFact.getComprobante("FACTURA");
+			IComprobante logo=new LogoDecorator(factura);
+			IComprobante logoFooter=new LogoDecorator(new LogoDecorator(factura));	
+			
+			
+			System.out.println("Que tipo de factura desea autorizar:\n"
+					+ "ONLINE\n"
+					+ "OFFLINE\n");
+			String tipoAutorizacion = sc.next();
+			
+			try {
+				AutorizacionFact esquemaFact = new AutorizacionFact();
+				Esquema esquema = esquemaFact.getEsquema(tipoAutorizacion);
+				AutorizadorSRI autorizacion = new AutorizadorSRI(esquema);
+				autorizacion.autorizar(factura);
+			} catch (Exception e) {
+				System.out.println("Usted ha creado una factura vacia");
+			}
+			System.out.println("Usted ha creado una factura vacia");
+			
 			break;
 		case 2:
 			ComprobanteElectronico guiaRemision = comprobanteFact.getComprobante("GUIAREMISION");
+			System.out.println("Ha creado una guia de remision");
 			break;
 		case 3:
 			ComprobanteElectronico notaCredito = comprobanteFact.getComprobante("NOTACREDITO");
+			System.out.println("Ha creado una nota de credito");
 			break;
 
 		default:
